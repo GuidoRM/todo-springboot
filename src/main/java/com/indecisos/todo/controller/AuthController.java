93f4fcba34cb690c01edefa51ddd2e00b264635c
GuidoRM <guidorafaelmamani100@gmail.com>
2024-10-13 19:56:11 -0300

    update: register controller

--- src/main/java/com/indecisos/todo/controller/AuthController.java
+++ src/main/java/com/indecisos/todo/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.indecisos.todo.controller;
 
+import com.indecisos.todo.dto.ApiResponse;
 import com.indecisos.todo.dto.AuthenticationRequest;
 import com.indecisos.todo.dto.AuthenticationResponse;
 import com.indecisos.todo.model.User;
@@ -7,12 +8,16 @@ import com.indecisos.todo.service.JwtUserDetailsService;
 import com.indecisos.todo.service.UserService;
 import com.indecisos.todo.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -32,12 +37,31 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping(value = "/register", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> registerUser(@RequestBody User user) {
+    public ResponseEntity<ApiResponse<User>> registerUser(@RequestBody User user) {
         try {
-            userService.save(user);
-            return ResponseEntity.ok("Usuario registrado con éxito");
+            // Asegúrate de que userWorkspaces esté inicializado
+            user.setUserWorkspaces(new HashSet<>());
+
+            // Establece otros campos necesarios
+            user.setCreationDate(LocalDateTime.now());
+            user.setIsAnonymous(false);  // o el valor que corresponda
+
+            User savedUser = userService.save(user);
+
+            ApiResponse<User> response = new ApiResponse<>(
+                    HttpStatus.CREATED.value(),
+                    "Usuario registrado con éxito",
+                    savedUser
+            );
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error al registrar el usuario: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(
+                            HttpStatus.BAD_REQUEST.value(),
+                            "Error al registrar el usuario: " + e.getMessage(),
+                            null
+                    ));
         }
     }
 
